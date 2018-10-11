71997637286879149393d865c37418aa98a3e39d
belo <belo.quintana@gmail.com>
2018-10-10 23:18:47 -0300

    Add Alert method - EmployeePage

--- AutomationFramework/PO/EmployeePage.cs
+++ AutomationFramework/PO/EmployeePage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using AutomationFramework.Handler;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,30 @@ namespace AutomationFramework.PO
 {
     public class EmployeePage : BasePage
     {
+        protected By Form = By.Id("formEmployee");
+        protected By NameInput = By.XPath("//*[@id='formEmployee']/div[2]/div[1]/input");
+        protected By EmailInput = By.XPath("//*[@id='formEmployee']/div[2]/div[2]/input");
+
         public EmployeePage(IWebDriver driver)
         {
             Driver = driver;
 
             if (Driver.Title.Equals("AUT From"))
                 throw new Exception("This is not the Employee page");
+        }
+        
+        public bool IsSuccessAlertPresent()
+        {
+            try
+            {
+                Driver.SwitchTo().Alert().Accept();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+            }
+
+            return false;
         }
     }
 }
